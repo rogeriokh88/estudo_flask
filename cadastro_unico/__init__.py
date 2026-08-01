a0c8f38af4bb7c1f16cb77f4fad4e34013423832
rogeriokh88 <rogeriokh88@gmail.com>
2026-08-01 12:48:25 -0300

    tela de login

--- cadastro_unico/__init__.py
+++ cadastro_unico/__init__.py
@@ -1,6 +1,13 @@
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_PROIFICATIONS"] = False
 
-from cadastro_unico.views import home
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from cadastro_unico.views import home ,login
--- cadastro_unico/views.py
+++ cadastro_unico/views.py
@@ -1,11 +1,17 @@
 from cadastro_unico import app
-from flask import render_template
+from flask import render_template,url_for
 
 @app.route("/")
 def home():
+    
+    context = {
+        "usuario" : "rogerio",
+        "idade" : 32 
+
+        }
     return render_template("index.html")
 
 
-@app.route("/nova/")
-def novapage():
-    return "nova pagina"
+@app.route("/login/")
+def login():
+    return render_template("login.html")
