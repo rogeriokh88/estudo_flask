5ecadd2ff2b05bd092ce563f02d3d66e522f9f8d
rogeriokh88 <rogeriokh88@gmail.com>
2026-08-06 20:13:37 -0300

    tela de relatorio

--- cadastro_unico/views.py
+++ cadastro_unico/views.py
@@ -15,10 +15,18 @@ def home():
 def agendar():
     return render_template("agendar.html")
 
-@app.route("/cadastro/")
-def cadastro():
-    return render_template("cadastro.html")
+@app.route("/cadastro_usuario/")
+def cadastro_usuario():
+    return render_template("cadastro_usuario.html")
 
 @app.route("/login/")
 def login():
     return render_template("login.html")
+
+@app.route("/relatorio/")
+def relatorio():
+    return render_template("relatorio.html")
+
+@app.route("/comprovante/")
+def comprovante():
+    return render_template("comprovante_do_agendamento.html")
